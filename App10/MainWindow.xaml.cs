d3ceb1c89257fcce110f45efd7d2ef54ce8c9103
choejaeho <choejaeho@F300>
2024-01-07 11:59:25 +0100

    초기 커밋.

---
+++ App10/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+using Microsoft.UI.Xaml;
+using AppUIBasics.Helper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Storage.Pickers;
+using Windows.Storage;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace App10
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Clear previous returned file name, if it exists, between iterations of this scenario
+            PickFilesOutputTextBlock.Text = "";
+
+            // Create a file picker
+            var openPicker = new Windows.Storage.Pickers.FileOpenPicker();
+
+            // Retrieve the window handle (HWND) of the current WinUI 3 window.
+            var window = WindowHelper.GetWindowForElement(this);
+            var hWnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            // Initialize the file picker with the window handle (HWND).
+            WinRT.Interop.InitializeWithWindow.Initialize(openPicker, hWnd);
+
+            // Set options for your file picker
+            openPicker.ViewMode = PickerViewMode.List;
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add("*");
+
+            // Open the picker for the user to pick a file
+            IReadOnlyList<StorageFile> files = await openPicker.PickMultipleFilesAsync();
+            if (files.Count > 0)
+            {
+                StringBuilder output = new StringBuilder("Picked files:\n");
+                foreach (StorageFile file in files)
+                {
+                    output.Append(file.Name + "\n");
+                }
+                PickFilesOutputTextBlock.Text = output.ToString();
+            }
+            else
+            {
+                PickFilesOutputTextBlock.Text = "Operation cancelled.";
+            }
+        }
+    }
+}
