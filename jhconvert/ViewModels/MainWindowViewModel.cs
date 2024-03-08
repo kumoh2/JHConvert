f520fef0f0a9986aff48926488d7ecfb65291bfa
Jaeho <kumoh2@naver.com>
2024-03-08 13:00:11 +0100

    devide main ui view and viewmodel

--- jhconvert/ViewModels/MainWindowViewModel.cs
+++ jhconvert/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using CommunityToolkit.WinUI.UI.Controls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using WinRT.Interop;
 
 namespace jhconvert.ViewModels
 {
     public class MainWindowViewModel
     {
+        public ObservableCollection<StackPanel> ColumnOptions { get; set; }
+        public ObservableCollection<object> ExcelData { get; set; }
+
+        public MainWindowViewModel()
+        {
+            ColumnOptions = new ObservableCollection<StackPanel>();
+            ExcelData = new ObservableCollection<object>();
+        }
+
+        public async Task LoadExcel(IntPtr hwnd, ListBox columnOptionsListBox, DataGrid excelDataGrid)
+        {
+            var picker = new FileOpenPicker();
+            InitializeWithWindow.Initialize(picker, hwnd);
+
+            picker.ViewMode = PickerViewMode.Thumbnail;
+            picker.FileTypeFilter.Add("*");
+            picker.FileTypeFilter.Add(".xlsx");
+            picker.FileTypeFilter.Add(".xlsm");
+            picker.FileTypeFilter.Add(".xlsb");
+            picker.FileTypeFilter.Add(".xls");
+
+            StorageFile file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Excel load start
+                var filePath = file.Path;
+                DataTable dataTable = ExcelHelper.LoadExcelIntoDataTable(filePath, true);
+                DataTableHelper.ColumnNametoListBox(dataTable, columnOptionsListBox);
+                AddActionsToListBox(columnOptionsListBox);
+                DataTableHelper.FillDataGrid(dataTable, excelDataGrid);
+            }
+        }
+
+        public void ExportExcel()
+        {
+            // Export Excel logic
+        }
+
+        public static void AddActionsToListBox(ListBox listBox)
+        {
+            var items = listBox.Items.Cast<TextBlock>().ToList();
+            listBox.Items.Clear();
+
+            foreach (var columnName in items)
+            {
+                StackPanel optionPanel = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 5, 0, 5) };
+
+                // 기존 TextBlock을 StackPanel에 추가
+                optionPanel.Children.Add(columnName);
+
+                ComboBox actionsComboBox = new ComboBox
+                {
+                    Width = 100,
+                    ItemsSource = new ObservableCollection<string> { "Keep", "Pivot", "Rename", "Delete" },
+                    SelectedIndex = 0
+                };
+                TextBox renameTextBox = new TextBox { Width = 100, Visibility = Visibility.Collapsed };
+                ComboBox pivotComboBox = new ComboBox { Width = 100, Visibility = Visibility.Collapsed };
+
+                actionsComboBox.SelectionChanged += (s, e) =>
+                {
+                    switch (actionsComboBox.SelectedItem as string)
+                    {
+                        case "Rename":
+                            renameTextBox.Visibility = Visibility.Visible;
+                            pivotComboBox.Visibility = Visibility.Collapsed;
+                            break;
+                        case "Pivot":
+                            renameTextBox.Visibility = Visibility.Collapsed;
+                            pivotComboBox.Visibility = Visibility.Visible;
+                            pivotComboBox.ItemsSource = new ObservableCollection<string>(listBox.Items.Cast<StackPanel>()
+                                .Select(panel => ((TextBlock)panel.Children[0]).Text));
+                            break;
+                        default:
+                            renameTextBox.Visibility = Visibility.Collapsed;
+                            pivotComboBox.Visibility = Visibility.Collapsed;
+                            break;
+                    }
+                };
+
+                optionPanel.Children.Add(actionsComboBox);
+                optionPanel.Children.Add(renameTextBox);
+                optionPanel.Children.Add(pivotComboBox);
+
+                listBox.Items.Add(optionPanel);
+            }
+        }
     }
 }
--- jhconvert/Views/MainWindow.xaml.cs
+++ jhconvert/Views/MainWindow.xaml.cs
@@ -1,15 +1,8 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using System;
-using System.Collections.ObjectModel;
-using System.Data;
-using Windows.Storage.Pickers;
-using Windows.Storage;
-using WinRT.Interop;
-using System.Linq;
 using jhconvert.ViewModels;
+using System;
 
-namespace jhconvert
+namespace jhconvert.Views
 {
     public partial class MainWindow : Window
     {
@@ -23,81 +16,13 @@ namespace jhconvert
 
         private async void LoadExcel_Click(object sender, RoutedEventArgs e)
         {
-            var picker = new FileOpenPicker();
-            IntPtr hwnd = WindowNative.GetWindowHandle(this);
-            InitializeWithWindow.Initialize(picker, hwnd);
-
-            picker.ViewMode = PickerViewMode.Thumbnail;
-            picker.FileTypeFilter.Add("*");
-            picker.FileTypeFilter.Add(".xlsx");
-            picker.FileTypeFilter.Add(".xlsm");
-            picker.FileTypeFilter.Add(".xlsb");
-            picker.FileTypeFilter.Add(".xls");
-
-            StorageFile file = await picker.PickSingleFileAsync();
-            if (file != null)
-            {
-                //Excel load start
-                var filePath = file.Path;
-                DataTable dataTable = ExcelHelper.LoadExcelIntoDataTable(filePath, true);
-                DataTableHelper.ColumnNametoListBox(dataTable, ColumnOptionsListBox);
-                AddActionsToListBox(ColumnOptionsListBox);
-                DataTableHelper.FillDataGrid(dataTable, ExcelDataGrid);
-            }
+            IntPtr hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+            await ViewModel.LoadExcel(hwnd, ColumnOptionsListBox, ExcelDataGrid);
         }
 
         private void ExportExcel_Click(object sender, RoutedEventArgs e)
-        { 
-        }
-
-        public static void AddActionsToListBox(ListBox listBox)
         {
-            var items = listBox.Items.Cast<TextBlock>().ToList();
-            listBox.Items.Clear();
-
-            foreach (var columnName in items)
-            {
-                StackPanel optionPanel = new StackPanel { Orientation = Orientation.Horizontal, Margin = new Thickness(0, 5, 0, 5) };
-
-                // 기존 TextBlock을 StackPanel에 추가
-                optionPanel.Children.Add(columnName);
-
-                ComboBox actionsComboBox = new ComboBox
-                {
-                    Width = 100,
-                    ItemsSource = new ObservableCollection<string> { "Keep", "Pivot", "Rename", "Delete" },
-                    SelectedIndex = 0
-                };
-                TextBox renameTextBox = new TextBox { Width = 100, Visibility = Visibility.Collapsed };
-                ComboBox pivotComboBox = new ComboBox { Width = 100, Visibility = Visibility.Collapsed };
-
-                actionsComboBox.SelectionChanged += (s, e) =>
-                {
-                    switch (actionsComboBox.SelectedItem as string)
-                    {
-                        case "Rename":
-                            renameTextBox.Visibility = Visibility.Visible;
-                            pivotComboBox.Visibility = Visibility.Collapsed;
-                            break;
-                        case "Pivot":
-                            renameTextBox.Visibility = Visibility.Collapsed;
-                            pivotComboBox.Visibility = Visibility.Visible;
-                            pivotComboBox.ItemsSource = new ObservableCollection<string>(listBox.Items.Cast<StackPanel>()
-                                .Select(panel => ((TextBlock)panel.Children[0]).Text));
-                            break;
-                        default:
-                            renameTextBox.Visibility = Visibility.Collapsed;
-                            pivotComboBox.Visibility = Visibility.Collapsed;
-                            break;
-                    }
-                };
-
-                optionPanel.Children.Add(actionsComboBox);
-                optionPanel.Children.Add(renameTextBox);
-                optionPanel.Children.Add(pivotComboBox);
-
-                listBox.Items.Add(optionPanel);
-            }
+            ViewModel.ExportExcel();
         }
     }
 }
