354dbbe852e5288e90dcf2a7f0fe63f83ac43a68
Jaeho <kumoh2@naver.com>
2024-03-16 22:59:01 +0100

    change winui3 -> winform

---
+++ jhconvert/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WinFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            button1 = new Button();
+            button2 = new Button();
+            checkedListBox1 = new CheckedListBox();
+            comboBox1 = new ComboBox();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 175F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 90F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(button1, 0, 0);
+            tableLayoutPanel1.Controls.Add(button2, 1, 0);
+            tableLayoutPanel1.Controls.Add(checkedListBox1, 0, 2);
+            tableLayoutPanel1.Controls.Add(comboBox1, 0, 1);
+            tableLayoutPanel1.Controls.Add(dataGridView1, 1, 1);
+            tableLayoutPanel1.Controls.Add(textBox1, 2, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 28F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 23F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Left;
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 22);
+            button1.TabIndex = 0;
+            button1.Text = "Load Excel";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Left;
+            button2.Location = new Point(178, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 22);
+            button2.TabIndex = 1;
+            button2.Text = "Export Excel";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // checkedListBox1
+            // 
+            checkedListBox1.Dock = DockStyle.Fill;
+            checkedListBox1.FormattingEnabled = true;
+            checkedListBox1.Location = new Point(3, 54);
+            checkedListBox1.Name = "checkedListBox1";
+            checkedListBox1.Size = new Size(169, 393);
+            checkedListBox1.TabIndex = 3;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Dock = DockStyle.Fill;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(2, 29);
+            comboBox1.Margin = new Padding(2, 1, 2, 1);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(171, 23);
+            comboBox1.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            tableLayoutPanel1.SetColumnSpan(dataGridView1, 2);
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(178, 31);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 82;
+            tableLayoutPanel1.SetRowSpan(dataGridView1, 2);
+            dataGridView1.Size = new Size(619, 416);
+            dataGridView1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(268, 3);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(96F, 96F);
+            AutoScaleMode = AutoScaleMode.Dpi;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private Button button1;
+        private Button button2;
+        private DataGridView dataGridView1;
+        private CheckedListBox checkedListBox1;
+        private ComboBox comboBox1;
+        private TextBox textBox1;
+    }
+}
---
+++ jhconvert/Form1.cs
@@ -0,0 +1,222 @@
+using System.Data;
+using ClosedXML.Excel;
+
+namespace WinFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        private DataTable originalDataTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+            checkedListBox1.ItemCheck += CheckedListBox1_ItemCheck; 
+            dataGridView1.RowPostPaint += dataGridView1_RowPostPaint;
+            textBox1.KeyPress += textBox1_KeyPress;
+            textBox1.TextChanged += textBox1_TextChanged;
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 숫자와 백스페이스만 허용
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // 빈 문자열이 아니어야 함
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox1.Text = "1";
+                textBox1.SelectionStart = textBox1.Text.Length; // 커서를 끝으로 이동
+            }
+        }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            using (SolidBrush b = new SolidBrush(dataGridView1.RowHeadersDefaultCellStyle.ForeColor))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font, b, e.RowBounds.Location.X + 20, e.RowBounds.Location.Y + 4);
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            using (var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Excel Files|*.xls;*.xlsx;*.xlsm;*.xlsb|All Files|*.*"
+            })
+            {
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var filePath = openFileDialog.FileName;
+                    originalDataTable = await Task.Run(() => LoadExcelIntoDataTable(filePath, true));
+                    PopulateColumnOptions(originalDataTable);
+                    dataGridView1.DataSource = originalDataTable;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "Excel Files|*.xlsx;*.xlsm;*.xlsb|All Files|*.*",
+                FileName = "Export.xlsx"
+            })
+            {
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    var filePath = saveFileDialog.FileName;
+                    int rowsPerFile = int.Parse(textBox1.Text);
+                    ExportDataGridViewToExcel(dataGridView1, filePath, rowsPerFile);
+                    MessageBox.Show("Data exported successfully!", "Export Excel", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private DataTable LoadExcelIntoDataTable(string filePath, bool firstRowAsColumnNames)
+        {
+            var dt = new DataTable();
+
+            using (var workbook = new XLWorkbook(filePath))
+            {
+                var worksheet = workbook.Worksheets.First();
+                var rows = worksheet.RangeUsed().RowsUsed();
+
+                var headerRow = rows.First();
+                foreach (var cell in headerRow.Cells())
+                {
+                    dt.Columns.Add(cell.GetString());
+                }
+
+                dt.BeginLoadData();
+                foreach (var row in rows.Skip(firstRowAsColumnNames ? 1 : 0))
+                {
+                    var dataRow = dt.NewRow();
+                    for (int i = 0; i < row.Cells().Count(); i++)
+                    {
+                        dataRow[i] = row.Cell(i + 1).GetString();
+                    }
+                    dt.Rows.Add(dataRow);
+                }
+                dt.EndLoadData();
+            }
+
+            return dt;
+        }
+
+        private void ExportDataGridViewToExcel(DataGridView dgv, string baseFilePath, int rowsPerFile)
+        {
+            int fileIndex = 1;
+            int totalRows = dgv.Rows.Count;
+            string directory = System.IO.Path.GetDirectoryName(baseFilePath);
+            string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(baseFilePath);
+            string extension = System.IO.Path.GetExtension(baseFilePath);
+
+            for (int startRow = 0; startRow < totalRows; startRow += rowsPerFile)
+            {
+                using (var workbook = new XLWorkbook())
+                {
+                    var worksheet = workbook.Worksheets.Add("Sheet1");
+
+                    // 열 헤더 추가
+                    AddHeadersToWorksheet(dgv, worksheet);
+
+                    // 행 데이터 추가
+                    bool hasData = AddRowsToWorksheet(dgv, worksheet, startRow, rowsPerFile);
+
+                    if (hasData)
+                    {
+                        string filePath = $"{directory}\\{fileNameWithoutExtension}_{fileIndex++}{extension}";
+                        workbook.SaveAs(filePath);
+                    }
+                }
+            }
+        }
+
+        private void AddHeadersToWorksheet(DataGridView dgv, IXLWorksheet worksheet)
+        {
+            for (int i = 0; i < dgv.Columns.Count; i++)
+            {
+                worksheet.Cell(1, i + 1).Value = dgv.Columns[i].HeaderText;
+            }
+        }
+
+        private bool AddRowsToWorksheet(DataGridView dgv, IXLWorksheet worksheet, int startRow, int rowsPerFile)
+        {
+            bool hasData = false;
+            for (int i = 0; i < rowsPerFile && (startRow + i) < dgv.Rows.Count; i++)
+            {
+                hasData = true;
+                for (int j = 0; j < dgv.Columns.Count; j++)
+                {
+                    worksheet.Cell(i + 2, j + 1).Value = dgv.Rows[startRow + i].Cells[j].Value?.ToString();
+                }
+            }
+            return hasData;
+        }
+
+        private void PopulateColumnOptions(DataTable dataTable)
+        {
+            checkedListBox1.Items.Clear();
+            comboBox1.Items.Clear();
+            foreach (DataColumn column in dataTable.Columns)
+            {
+                checkedListBox1.Items.Add(column.ColumnName);
+                comboBox1.Items.Add(column.ColumnName);
+            }
+        }
+
+        private void CheckedListBox1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            if (comboBox1.SelectedItem == null) return;
+
+            var selectedColumn = comboBox1.SelectedItem.ToString();
+            var checkedColumns = checkedListBox1.CheckedItems.Cast<string>().ToList();
+
+            if (e.NewValue == CheckState.Checked)
+            {
+                checkedColumns.Add(checkedListBox1.Items[e.Index].ToString());
+            }
+            else if (e.NewValue == CheckState.Unchecked)
+            {
+                checkedColumns.Remove(checkedListBox1.Items[e.Index].ToString());
+            }
+
+            DataTable updatedDataTable = new DataTable();
+
+            // 새 데이터 테이블에 원본 컬럼 추가
+            updatedDataTable.Columns.Add(selectedColumn);
+            foreach (DataColumn column in originalDataTable.Columns)
+            {
+                if (column.ColumnName != selectedColumn && !checkedColumns.Contains(column.ColumnName))
+                {
+                    updatedDataTable.Columns.Add(column.ColumnName);
+                }
+            }
+
+            foreach (DataRow row in originalDataTable.Rows)
+            {
+                foreach (var column in checkedColumns)
+                {
+                    DataRow newRow = updatedDataTable.NewRow();
+                    newRow[selectedColumn] = row[column];
+                    foreach (DataColumn originalColumn in originalDataTable.Columns)
+                    {
+                        if (originalColumn.ColumnName != selectedColumn && !checkedColumns.Contains(originalColumn.ColumnName))
+                        {
+                            newRow[originalColumn.ColumnName] = row[originalColumn.ColumnName];
+                        }
+                    }
+                    updatedDataTable.Rows.Add(newRow);
+                }
+            }
+
+            dataGridView1.DataSource = updatedDataTable;
+        }
+    }
+}
